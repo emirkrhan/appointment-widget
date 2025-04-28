b31977edfd0089a9eb87b32c8b8008a7bfcf8a55
Emirhan Körhan <korhanemirhann@gmail.com>
2025-04-29 00:57:33 +0300

    first

---
+++ public/widget.js
@@ -0,0 +1,6 @@
+(function () {
+    const script = document.createElement('script');
+    script.src = '/_next/static/chunks/pages/widget.js';
+    script.async = true;
+    document.head.appendChild(script);
+  })();
